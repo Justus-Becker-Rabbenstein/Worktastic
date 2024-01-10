f265ffd17b7212aa6bf69563e1328309a5415929
Justus Becker-Rabbenstein <justusbera@googlemail.com>
2024-01-10 14:49:41 +0100

    JobPosting Model erstellen und patchen

--- Worktastic/Data/ApplicationDbContext.cs
+++ Worktastic/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Worktastic.Models;
 
 namespace Worktastic.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<JobPosting> JobPostings { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
