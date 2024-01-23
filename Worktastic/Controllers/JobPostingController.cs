73b29076426e72f61da0a6ee0fd07061a042d020
Justus Becker-Rabbenstein <justusbera@googlemail.com>
2024-01-23 15:01:04 +0100

    JobPosting erstellen und überschreiben (Create und Edit)

--- Worktastic/Controllers/JobPostingController.cs
+++ Worktastic/Controllers/JobPostingController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
+using System.Linq;
+using Worktastic.Data;
 using Worktastic.Models;
 
 namespace Worktastic.Controllers
 {
     public class JobPostingController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public JobPostingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -28,6 +36,32 @@ namespace Worktastic.Controllers
                     jobPosting.CompanyImage = bytes;
                 }
             }
+
+            if (jobPosting.Id == 0)
+            {
+                // Add new job if not editing
+                _context.JobPostings.Add(jobPosting);
+            }
+            else
+            {
+                var jobFromDb = _context.JobPostings.SingleOrDefault(x => x.Id == jobPosting.Id);
+
+                if (jobFromDb == null)
+                {
+                    return NotFound();
+                }
+                jobFromDb.CompanyImage = jobPosting.CompanyImage;
+                jobFromDb.CompanyName = jobPosting.CompanyName;
+                jobFromDb.ContactMail = jobPosting.ContactMail;
+                jobFromDb.ContactPhone = jobPosting.ContactPhone;
+                jobFromDb.ContactWebsite = jobPosting.ContactWebsite;
+                jobFromDb.Description = jobPosting.Description;
+                jobFromDb.JobLocation = jobPosting.JobLocation;
+                jobFromDb.JobTitle = jobPosting.JobTitle;
+                jobFromDb.Salary = jobPosting.Salary;
+                jobFromDb.StartDate = jobPosting.StartDate;
+            }
+            _context.SaveChanges();
 
             return RedirectToAction("Index");
         }
